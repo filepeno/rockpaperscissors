e494e2d032e27c3b75d1b6143b1f71cec320cd57
Filip <fili4252@stud.kea.dk>
2021-08-26 11:26:06 +0200

    made js scaffolding

---
+++ mechanics.js
@@ -0,0 +1,42 @@
+//debugging
+"use strict";
+
+//start game
+function start() {
+  console.log("start");
+}
+
+//get player choice
+function playerChoice() {
+  console.log("playerChoice");
+}
+
+//make computer choice
+function computerChoice() {
+  console.log("computerChoice");
+}
+
+//run the game animation
+function showAnimations() {
+  console.log("showAnimations");
+}
+
+//determine the winner
+function whoWon() {
+  console.log("whoWon");
+}
+
+//go to the winner screen
+function showWin() {
+  console.log("showWin");
+}
+
+//got to the losing screen
+function showLose() {
+  console.log("showLose");
+}
+
+//go to the draw screen
+function showDraw() {
+  console.log("showDraw");
+}
